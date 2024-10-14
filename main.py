fd93674e30f8d1ecc633ccfef8732d7a53a43d1b
GoStevenGo <stevenpaulgomez@gmail.com>
2024-10-14 14:29:39 -0500

    added Player class file, and drew an instance of player spaceship to screen

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    spaceship = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) #instance of Player class
 
     while True: #Game loop
-        #Makes the window's close button work
-        for event in pygame.event.get():
+        for event in pygame.event.get(): #Makes the window's close button work
             if event.type == pygame.QUIT:
                 return
         
-        #Creates game window/screen
-        screen.fill("black")
+        screen.fill("black") #Creates game window/screen
+        spaceship.draw(screen) #rendering the player on screen each frame using the .draw method from Player class
         pygame.display.flip()
 
-        #limit the framerate to 60 FPS
-        dt = clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000 #limit the framerate to 60 FPS
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
