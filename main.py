510123511e01c8e30f1c150ce6c5288e8920accf
GoStevenGo <stevenpaulgomez@gmail.com>
2024-10-14 15:12:31 -0500

    created pygame groups, added instances of Player class to these groups, and added methods that allow for user input to move/rotate Player spaceship

--- main.py
+++ main.py
@@ -7,22 +7,32 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable) #adding all future Player instances to these groups
+
     spaceship = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) #instance of Player class
 
     while True: #Game loop
         for event in pygame.event.get(): #Makes the window's close button work
             if event.type == pygame.QUIT:
                 return
-        
+
+        for object in updatable:
+            object.update(dt) #checks for user input (WASD) for player rotation and movement 
+
         screen.fill("black") #Creates game window/screen
-        spaceship.draw(screen) #rendering the player on screen each frame using the .draw method from Player class
-        pygame.display.flip()
+
+        for object in drawable:
+            object.draw(screen) #Creates the player on screen each frame using the .draw method from Player class
+
+        pygame.display.flip() #renders everything above
 
         dt = clock.tick(60) / 1000 #limit the framerate to 60 FPS
 
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
 
 if __name__ == "__main__":
     main()
